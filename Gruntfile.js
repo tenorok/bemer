f1d1d21e6fcae309e0145cca81646f8132dc04f7
tenorok <mail@tenorok.ru>
2014-02-23 22:10:43 +0400

    Gruntfile настроен на юнит-тесты

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,42 @@
 module.exports = function(grunt) {
 
+    var module = grunt.option('module') || 'main';
+
     grunt.initConfig({
         clean: {
-            hooks: ['.git/hooks/*']
+            hooks: ['.git/hooks/*'],
+            test: ['!test/tmp/.gitkeep', 'test/tmp/*']
         },
         shell: {
             hooks: {
                 command: 'cp .githooks/* .git/hooks/'
+            }
+        },
+        definer: {
+            main: {
+                target: 'test/tmp/main.js',
+                directory: ['modules/', 'test/']
+            },
+            name: {
+                module: 'NameTest',
+                target: 'test/tmp/name.js',
+                directory: ['modules/', 'test/']
+            }
+        },
+        mochaTest: {
+            main: {
+                src: ['test/tmp/*']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-definer');
+    grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.registerTask('install-hooks', ['clean:hooks', 'shell:hooks']);
+
+    grunt.registerTask('test', ['clean:test', 'definer:' + module, 'mochaTest']);
 
 };
