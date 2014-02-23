6658730bb7b411c0c94e1a8268bcaa5ebe3d6c3b
tenorok <mail@tenorok.ru>
2014-02-23 22:45:10 +0400

    Тесты на модуль Name

---
+++ test/name.js
@@ -0,0 +1,51 @@
+definer('NameTest', function(Name) {
+    var assert = require('chai').assert;
+
+    describe('Модуль Name.', function() {
+
+        it('Получить имя блока', function() {
+            var entity = new Name('block');
+            assert.equal(entity.info().block, 'block');
+        });
+
+        it('Получить имя модификатора блока', function() {
+            var entity = new Name('block_mod');
+            assert.equal(entity.info().modName, 'mod');
+        });
+
+        it('Получить имя и значение модификатора блока', function() {
+            var entity = new Name('block_mod_val');
+            assert.equal(entity.info().modName, 'mod');
+            assert.equal(entity.info().modVal, 'val');
+        });
+
+        it('Получить имя элемента', function() {
+            var entity = new Name('block__element');
+            assert.equal(entity.info().elem, 'element');
+        });
+
+        it('Получить имя модификатора элемента', function() {
+            var entity = new Name('block__element_elemMod');
+            assert.equal(entity.info().elemModName, 'elemMod');
+        });
+
+        it('Получить имя и значение модификатора элемента', function() {
+            var entity = new Name('block__element_elemMod_elemVal');
+            assert.equal(entity.info().elemModName, 'elemMod');
+            assert.equal(entity.info().elemModVal, 'elemVal');
+        });
+
+        it('Получить всю информацию о сущности', function() {
+            var entity = new Name('block_mod_val__element_elemMod_elemVal');
+            assert.equal(entity.info(), {
+                block: 'block',
+                modName: 'mod',
+                modVal: 'val',
+                elem: 'element',
+                elemModName: 'elemMod',
+                elemModVal: 'elemVal'
+            });
+        });
+
+    });
+});
