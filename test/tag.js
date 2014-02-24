bf5c6e1b41e82e3660428fa76faddbd5f96d1c85
tenorok <mail@tenorok.ru>
2014-02-24 22:45:36 +0400

    Тест на установку/получение имени тега

---
+++ test/tag.js
@@ -0,0 +1,14 @@
+definer('TagTest', function(assert, Tag) {
+    describe('Модуль Tag.', function() {
+
+        it('Получить имя тега', function() {
+            var div = new Tag();
+            assert.equal(div.name(), 'div');
+
+            var poly = new Tag('span');
+            assert.equal(poly.name(), 'span');
+            assert.equal(poly.name('img').name(), 'img');
+        });
+
+    });
+});
