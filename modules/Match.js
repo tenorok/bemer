3c54777e5e21f263b9fd4491fde54417dd987e27
tenorok <mail@tenorok.ru>
2014-03-01 22:56:17 +0400

    Выполняется проверка на шаблоны с элементами

--- modules/Match.js
+++ modules/Match.js
@@ -17,6 +17,13 @@ definer('Match', /** @exports Match */ function(Name) {
         this._pattern = new Name(pattern);
     }
 
+    /**
+     * Символ любого значения.
+     *
+     * @type {string}
+     */
+    Match.any = '*';
+
     Match.prototype = {
 
         /**
@@ -26,7 +33,36 @@ definer('Match', /** @exports Match */ function(Name) {
          * @returns {boolean}
          */
         is: function(node) {
-            return this._pattern.block() === node.block;
+            return (this._block(node.block) && this._elem(node.elem));
+        },
+
+        /**
+         * Проверить блок на соответствие шаблону.
+         *
+         * @private
+         * @param {string} block Имя блока узла
+         * @returns {boolean}
+         */
+        _block: function(block) {
+            var pattern = this._pattern.block();
+            return pattern === Match.any || pattern === block;
+        },
+
+        /**
+         * Проверить элемент на соответствие шаблону.
+         *
+         * @private
+         * @param {string} elem Имя элемента узла
+         * @returns {boolean}
+         */
+        _elem: function(elem) {
+            var pattern = this._pattern.elem();
+
+            if(!pattern && !elem) {
+                return true;
+            }
+
+            return pattern === Match.any || pattern === elem;
         }
 
     };
--- test/MatchTest.js
+++ test/MatchTest.js
@@ -2,9 +2,42 @@ definer('MatchTest', function(assert, Match) {
     describe('Модуль Match.', function() {
 
         it('Проверить на соответствие блоку', function() {
-            var match = new Match('my-block');
-            assert.isTrue(match.is({ block: 'my-block' }));
-            assert.isFalse(match.is({ block: 'not-my-block' }));
+            var match = new Match('block');
+            assert.isTrue(match.is({ block: 'block' }));
+            assert.isFalse(match.is({ block: 'not-block' }));
+        });
+
+        it('Проверить на соответствие любому блоку', function() {
+            assert.isTrue(new Match('*').is({ block: 'block' }));
+        });
+
+        it('Проверить на соответствие элементу', function() {
+            var match = new Match('block__element');
+            assert.isTrue(match.is({ block: 'block', elem: 'element' }));
+            assert.isFalse(match.is({ block: 'not-block', elem: 'element' }));
+            assert.isFalse(match.is({ block: 'block', elem: 'not-element' }));
+            assert.isFalse(match.is({ block: 'not-block', elem: 'not-element' }));
+        });
+
+        it('Проверить на соответствие любому блоку с элементом', function() {
+            var match = new Match('*__element');
+            assert.isTrue(match.is({ block: 'any-block', elem: 'element' }));
+            assert.isFalse(match.is({ block: 'block', elem: 'not-element' }));
+        });
+
+        it('Проверить на соответствие любому элементу', function() {
+            var match = new Match('block__*');
+            assert.isTrue(match.is({ block: 'block', elem: 'element' }));
+            assert.isFalse(match.is({ block: 'not-block', elem: 'not-element' }));
+        });
+
+        it('Проверить блок с элементом на любое соответствие', function() {
+            assert.isFalse(new Match('*').is({ block: 'block', elem: 'element' }));
+
+            var match = new Match('*__*');
+            assert.isTrue(match.is({ block: 'block', elem: 'element' }));
+            assert.isTrue(match.is({ block: 'not-block', elem: 'element' }));
+            assert.isTrue(match.is({ block: 'not-block', elem: 'not-element' }));
         });
 
     });
