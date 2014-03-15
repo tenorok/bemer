a7678b7161d6944b69e6e112b1b157f52d7908d0
tenorok <mail@tenorok.ru>
2014-03-15 16:46:00 +0400

    Модуль Template наследует шаблоны от базового и работает с модами

--- modules/Template.js
+++ modules/Template.js
@@ -1,4 +1,4 @@
-definer('Template', /** @exports Template */ function(Match, classify, Node) {
+definer('Template', /** @exports Template */ function(Match, classify, Node, object) {
 
     /**
      * Модуль шаблонизации BEMJSON-узла.
@@ -23,19 +23,98 @@ definer('Template', /** @exports Template */ function(Match, classify, Node) {
          * @private
          * @type {Function}
          */
-        this._Modes = classify(modes);
+        this._Modes = classify(Template.Base, modes);
     }
+
+    /**
+     * Стандартные моды.
+     *
+     * @type {object}
+     */
+    Template.Modes = {
+        js: true,
+        bem: true,
+        mods: {},
+        elemMods: {},
+        attrs: {},
+        mix: [],
+        tag: 'div',
+        cls: '',
+        content: ''
+    };
+
+    /**
+     * Базовый шаблон со стандартными модами.
+     *
+     * @type {Function}
+     */
+    Template.Base = classify(object.clone(Template.Modes));
 
     Template.prototype = {
 
         /**
          * Применить BEMJSON к шаблону.
          *
-         * @param {object} bemjson BEMJSON
+         * @param {object} bemjson Входящий BEMJSON
          * @returns {Node|null} Экземпляр БЭМ-узла или null при несоответствии BEMJSON шаблону
          */
         match: function(bemjson) {
-            return this._match.is(bemjson) ? new Node(bemjson) : null;
+            return this._match.is(bemjson) ? this.transform(bemjson) : null;
+        },
+
+        /**
+         * Преобразовать BEMJSON.
+         *
+         * @param {object} bemjson Входящий BEMJSON
+         * @returns {Node}
+         */
+        transform: function(bemjson) {
+            var modes = new this._Modes(bemjson);
+
+            Object.keys(Template.Modes).forEach(function(mode) {
+                bemjson[mode] = this._getMode(modes, bemjson, mode);
+            }, this);
+
+            return new Node(bemjson);
+        },
+
+        /**
+         * Получить значение моды.
+         *
+         * @private
+         * @param {Object} modes Экземпляр класса по модам
+         * @param {object} bemjson Входящий BEMJSON
+         * @param {string} name Имя требуемой моды
+         * @returns {*}
+         */
+        _getMode: function(modes, bemjson, name) {
+            var val = typeof modes[name] === 'function' ? modes[name].call(modes) : modes[name],
+                bemjsonVal = bemjson[name];
+
+            this._checkTypes(typeof Template.Modes[name], [val, bemjsonVal], name);
+
+            if(typeof val === 'object') {
+                return object.extend(val, bemjsonVal || {});
+            }
+
+            return bemjsonVal || val;
+        },
+
+        /**
+         * Проверить тип данных мод.
+         *
+         * @private
+         * @param {string} valid Эталонный тип
+         * @param {*[]} values Список значений к проверке
+         * @param {string} name Имя моды для вывода отладочной ошибки
+         * @throws {TypeError} Неверный тип данных
+         */
+        _checkTypes: function(valid, values, name) {
+            values.forEach(function(val) {
+                if(val !== undefined && typeof val !== valid) {
+                    throw new TypeError(val + ' is wrong type of mode ' + name);
+                }
+            });
         }
 
     };
--- test/TemplateTest.js
+++ test/TemplateTest.js
@@ -2,7 +2,9 @@ definer('TemplateTest', function(assert, Template) {
     describe('Модуль Template.', function() {
 
         it('Шаблонизировать простой блок', function() {
-            assert.equal(new Template('name', {}).match({ block: 'name' }).toString(), '<div class="name"></div>');
+            assert.equal(new Template('name', {}).match({ block: 'name' }).toString(),
+                '<div class="name i-bem" data-bem="{&quot;name&quot;:{}}"></div>'
+            );
             assert.isNull(new Template('name', {}).match({ block: 'name2' }));
         });
 
@@ -10,8 +12,24 @@ definer('TemplateTest', function(assert, Template) {
             assert.equal(new Template('name__element', {}).match({
                 block: 'name',
                 elem: 'element'
-            }).toString(), '<div class="name__element"></div>');
+            }).toString(), '<div class="name__element i-bem" data-bem="{&quot;name__element&quot;:{}}"></div>');
             assert.isNull(new Template('name__element2', {}).match({ block: 'name', elem: 'element' }));
+        });
+
+        it('Шаблонизировать блок и элемент с заменой тега', function() {
+            assert.equal(new Template('name', {
+                tag: 'span'
+            }).match({ block: 'name' }).toString(),
+                '<span class="name i-bem" data-bem="{&quot;name&quot;:{}}"></span>'
+            );
+        });
+
+        it('Шаблонизировать элемент с заменой тега', function() {
+            assert.equal(new Template('my__*', {
+                tag: function() { return 'img'; }
+            }).match({ block: 'my', elem: 'image' }).toString(),
+                '<img class="my__image i-bem" data-bem="{&quot;my__image&quot;:{}}"/>'
+            );
         });
 
     });
