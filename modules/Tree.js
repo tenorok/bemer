c12de71ceb2c9c9b4978f66dbb00c991acbd1e67
tenorok <mail@tenorok.ru>
2014-04-06 00:21:51 +0400

    Модуль Tree способен шаблонизировать сущности по стандартным шаблонам, если специальных шаблонов не было найдено в списке

--- modules/Tree.js
+++ modules/Tree.js
@@ -1,11 +1,11 @@
-definer('Tree', /** @exports Tree */ function() {
+definer('Tree', /** @exports Tree */ function(Template) {
 
     /**
      * Модуль работы с BEMJSON-деревом.
      *
      * @class
      */
-    function Tree(tree) {
+    function Tree(tree, pool) {
 
         /**
          * Дерево.
@@ -14,11 +14,56 @@ definer('Tree', /** @exports Tree */ function() {
          * @type {object}
          */
         this._tree = tree;
+
+        /**
+         * Список шаблонов.
+         *
+         * @private
+         * @type {Pool}
+         */
+        this._pool = pool;
+
+        /**
+         * Контекст блока в дереве.
+         *
+         * @private
+         * @type {string}
+         */
+        this._block = tree.block || '';
     }
+
+    /**
+     * Стандартные шаблоны.
+     *
+     * @type {object}
+     * @property {Template} block Блока
+     * @property {Template} elem Элемента
+     * @property {Template} blockModElem Блока с модификатором и элемента
+     */
+    Tree.defaultTemplates = {
+        block: new Template('*', {}),
+        elem: new Template('*__*', {}),
+        blockModElem: new Template('*_*_*__*', {})
+    };
 
     Tree.prototype = {
 
-        expand: function() {}
+        expand: function() {},
+
+        toString: function() {
+            var node = this._pool.find(this._tree);
+
+            if(node) {
+                return node.toString();
+            }
+
+            for(var key in Tree.defaultTemplates) {
+                node = Tree.defaultTemplates[key].match(this._tree);
+                if(node) {
+                    return node.toString();
+                }
+            }
+        }
 
     };
 
--- test/TreeTest.js
+++ test/TreeTest.js
@@ -1,5 +1,50 @@
-definer('TreeTest', function(assert, Tree) {
+definer('TreeTest', function(assert, Tree, Pool, Template) {
     describe('Модуль Tree.', function() {
+
+        describe('Сущности без шаблонов.', function() {
+
+            it('Блок', function() {
+                var tree = new Tree({ block: 'a' }, new Pool());
+                assert.equal(tree.toString(), '<div class="a i-bem" data-bem="{&quot;a&quot;:{}}"></div>');
+            });
+
+            it('Блок с модификатором', function() {
+                var tree = new Tree({ block: 'a', mods: { b: 'c' }}, new Pool());
+                assert.equal(tree.toString(), '<div class="a i-bem a_b_c" data-bem="{&quot;a&quot;:{}}"></div>');
+            });
+
+            it('Элемент', function() {
+                var tree = new Tree({ block: 'a', elem: 'b' }, new Pool());
+                assert.equal(tree.toString(), '<div class="a__b i-bem" data-bem="{&quot;a__b&quot;:{}}"></div>');
+            });
+
+            it('Элемент с модификатором', function() {
+                var tree = new Tree({ block: 'a', elem: 'b', elemMods: { c: 'd' }}, new Pool());
+                assert.equal(tree.toString(),
+                    '<div class="a__b i-bem a__b_c_d" data-bem="{&quot;a__b&quot;:{}}"></div>'
+                );
+            });
+
+            it('Блок с модификатором и элементом', function() {
+                var tree = new Tree({ block: 'a', mods: { c: 'd' }, elem: 'b' }, new Pool());
+                assert.equal(tree.toString(),
+                    '<div class="i-bem a_c_d__b" data-bem="{&quot;a__b&quot;:{}}"></div>'
+                );
+            });
+
+            it('Блок с модификатором и элемент с модификатором', function() {
+                var tree = new Tree({ block: 'a', mods: { c: 'd' }, elem: 'b', elemMods: { e: 'f' }}, new Pool());
+                assert.equal(tree.toString(),
+                    '<div class="i-bem a_c_d__b a_c_d__b_e_f" data-bem="{&quot;a__b&quot;:{}}"></div>'
+                );
+            });
+
+        });
+
+        it('Один простой блок', function() {
+            var tree = new Tree({ block: 'a' }, new Pool().add(new Template('a', {})));
+            assert.equal(tree.toString(), '<div class="a i-bem" data-bem="{&quot;a&quot;:{}}"></div>');
+        });
 
         it('Раскрыть контекст блока для вложенного элемента', function() {
             var tree = new Tree({
@@ -7,15 +52,8 @@ definer('TreeTest', function(assert, Tree) {
                 content: {
                     elem: 'b'
                 }
-            });
-
-            assert.deepEqual(tree.expand(), {
-                block: 'a',
-                content: {
-                    block: 'a',
-                    elem: 'b'
-                }
-            });
+            }, new Pool().add(new Template('a', { js: false })));
+            assert.equal(tree.toString(), '<div class="a"><div class="a__b"></div></div>');
         });
 
     });
