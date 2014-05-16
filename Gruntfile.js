e11143167f6e53f4ad626b344d24df54149d63db
tenorok <mail@tenorok.ru>
2014-05-16 15:14:32 +0400

    В грант добавлена цель для сборки релиза

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,23 @@ module.exports = function(grunt) {
         module = grunt.option('module') || 'main';
 
     grunt.initConfig({
+        mkdir: {
+            release: {
+                options: { create: ['release'] }
+            }
+        },
+        uglify: {
+            release: {
+                options: {
+                    preserveComments: 'some'
+                },
+                files: { 'release/bemer.min.js': 'release/bemer.js' }
+            }
+        },
         clean: {
             githooks: ['.git/hooks/*'],
-            test: ['!test/tmp/.gitkeep', 'test/tmp/*']
+            test: ['!test/tmp/.gitkeep', 'test/tmp/*'],
+            release: ['release']
         },
         shell: {
             githooks: { command: 'cp .githooks/* .git/hooks/' },
@@ -18,8 +32,10 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-definer');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('githooks', ['clean:githooks', 'shell:githooks']);
     grunt.registerTask('jsdoc', ['shell:jsdoc']);
@@ -29,5 +45,10 @@ module.exports = function(grunt) {
         'definer:' + module,
         'mochaTest'
     ]);
+
+    grunt.registerTask('release', function() {
+        grunt.task.run('test');
+        grunt.task.run(['clean:release', 'mkdir:release', 'definer:release', 'uglify:release']);
+    });
 
 };
