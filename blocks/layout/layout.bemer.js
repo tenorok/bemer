85cf85f921218b96cb395996f694a0e15f7be9bb
tenorok <mail@tenorok.ru>
2014-06-16 12:31:39 +0400

    Расставлены ссылки для дополнительной информации

--- blocks/layout/layout.bemer.js
+++ blocks/layout/layout.bemer.js
@@ -67,7 +67,11 @@ bemer.match('layout', {
             content: [
                 {
                     elem: 'templates',
-                    title: 'Декларируем шаблоны',
+                    title: {
+                        block: 'link',
+                        href: 'https://github.com/tenorok/bemer#%D0%9C%D0%B5%D1%82%D0%BE%D0%B4-match',
+                        content: 'Декларируем шаблоны'
+                    },
                     content: {
                         block: 'ace',
                         mode: 'javascript'
@@ -90,7 +94,11 @@ bemer.match('layout', {
                 },
                 {
                     elem: 'result',
-                    title: 'Получаем результирующий HTML',
+                    title: {
+                        block: 'link',
+                        href: 'https://github.com/tenorok/bemer#%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D1%80%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%D0%B0',
+                        content: 'Получаем результирующий HTML'
+                    },
                     content: {
                         block: 'ace',
                         mode: 'html'
