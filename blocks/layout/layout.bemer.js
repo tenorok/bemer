60dee778ba17314387d1298ea507c9f65f1385fd
tenorok <mail@tenorok.ru>
2014-08-02 22:33:06 +0400

    v0.4.0

--- blocks/layout/layout.bemer.js
+++ blocks/layout/layout.bemer.js
@@ -60,7 +60,7 @@ bemer.match('layout', {
                 },
                 {
                     block: 'install',
-                    version: '0.3.1',
+                    version: '0.4.0',
                     size: {
                         min: 20
                     }
