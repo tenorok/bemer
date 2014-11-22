b176b342349b1c857948f059fff27b1d5ceef697
tenorok <mail@tenorok.ru>
2014-11-23 00:19:28 +0400

    Увеличена ширина редактора декларации шаблонов и уменьшена ширина редактора bemjson

--- blocks/layout/layout.bemer.js
+++ blocks/layout/layout.bemer.js
@@ -89,10 +89,10 @@ bemer.match('layout', {
                 {
                     elem: 'bemjson',
                     title: [
-                        'Подаём входящие данные в формате ',
+                        'Подаём на вход ',
                         {
                             block: 'link',
-                            href: 'http://ru.bem.info/tools/templating-engines/BEMHTML/#bemjson',
+                            href: 'http://ru.bem.info/technology/bemjson/2.3.0/bemjson/#Синтаксис-BEMJSON',
                             content: 'BEMJSON'
                         }
                     ],
@@ -110,7 +110,7 @@ bemer.match('layout', {
                     title: {
                         block: 'link',
                         href: 'https://github.com/tenorok/bemer#%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D1%80%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%D0%B0',
-                        content: 'Получаем результирующий HTML'
+                        content: 'Получаем HTML'
                     },
                     content: {
                         block: 'ace',
